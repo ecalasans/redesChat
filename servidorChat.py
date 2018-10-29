c2d5961e1f0dd17742eaa38df777b0e5fac1a0e7
ecalasans <e1906d0103>
2018-10-29 16:46:17 -0300

    ClienteChat em construção

--- servidorChat.py
+++ servidorChat.py
@@ -51,7 +51,7 @@ class ServidorChat:
             print('{}:{} conectou-se!'.format(clienteEndereco[0], clienteEndereco[1]))
 
             #Solicita o nick ao cliente
-            solNick = Mensagem(str(16 + len('Digite seu nick:  ')), self.HOST_INTERFACE_REDE, clienteEndereco[0],
+            solNick = Mensagem(str(16 + len('Digite seu nick, porra:  ')), self.HOST_INTERFACE_REDE, clienteEndereco[0],
                                'serv', 'nick()', 'Digite seu nick:  ')
             clienteSocket.send(solNick.getMensagemCompleta().encode('utf-8'))
 
